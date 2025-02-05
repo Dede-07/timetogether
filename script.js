2ef2b6918846e3a3b35ade6cf8627d100d4998a3
Andre_Carrijo <161145988+Dede-07@users.noreply.github.com>
2025-02-05 12:42:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const dataInicial = new Date(2024, 8, 7);
 
 function calcularTempoJuntos() {
-    const hoje = new Date();
-    let anos = hoje.getFullYear() - dataInicial.getFullYear();
-    let meses = hoje.getMonth() - dataInicial.getMonth();
-    let dias = hoje.getDate() - dataInicial.getDate();
+    const agora = new Date();
+    let diferenca = agora - dataInicial;
+
+    let anos = agora.getFullYear() - dataInicial.getFullYear();
+    let meses = agora.getMonth() - dataInicial.getMonth();
+    let dias = agora.getDate() - dataInicial.getDate();
 
     if (dias < 0) {
         meses--;
-        const ultimoMes = new Date(hoje.getFullYear(), hoje.getMonth(), 0).getDate();
+        const ultimoMes = new Date(agora.getFullYear(), agora.getMonth(), 0).getDate();
         dias += ultimoMes;
     }
 
@@ -17,7 +19,19 @@ function calcularTempoJuntos() {
         meses += 12;
     }
 
-    document.getElementById('contador').textContent = `${anos} anos, ${meses} meses, ${dias} dias`;
+    let segundosTotais = Math.floor(diferenca / 1000);
+    let minutosTotais = Math.floor(segundosTotais / 60);
+    let horasTotais = Math.floor(minutosTotais / 60);
+
+    let horas = horasTotais % 24;
+    let minutos = minutosTotais % 60;
+    let segundos = segundosTotais % 60;
+
+
+    document.getElementById('contador').innerHTML =
+        `${anos} anos, ${meses} meses, ${dias} dias, <br>` +
+        `${horasTotais} horas, ${minutos} minutos e ${segundos} segundos`;
 }
 
+setInterval(calcularTempoJuntos, 1000);
 window.onload = calcularTempoJuntos;
