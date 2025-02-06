1b4824d29e9ac980479f54add4f3f8c6bee895ef
Andre_Carrijo <161145988+Dede-07@users.noreply.github.com>
2025-02-06 10:37:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ function calcularTempoJuntos() {
 
 setInterval(calcularTempoJuntos, 1000);
 window.onload = calcularTempoJuntos;
+
+const heartCreate = () => {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.innerText = "❤️";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+};
+
+setInterval(heartCreate, 300);
